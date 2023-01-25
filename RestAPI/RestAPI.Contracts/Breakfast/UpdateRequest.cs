c938f6c8e32d29a9b2b8c0167a0440484a4a4714
GreeshmaMary <greeshma.mary.josphin@networkredux.com>
2023-01-25 12:42:28 -0700

    Check
    Check

---
+++ RestAPI/RestAPI.Contracts/Breakfast/UpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPI.Contracts.Breakfast
+{
+    public record UpdateRequest
+    (
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EndDateTime,
+        List<string> Savory,
+        List<string> Sweet
+
+    );
+}
---
+++ RestAPI/RestAPI/Controllers/BreakfastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestAPI.Contracts.Breakfast;
+
+namespace RestAPI.Controllers
+{
+    [ApiController]
+    public class BreakfastController : ControllerBase
+    {
+        [HttpPost("/breakfast")]
+        public IActionResult CreateBreakfast(CreateRequest breakfastRequest)
+        {
+            return Ok(breakfastRequest);
+        }
+
+        [HttpGet("/breakfast/{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("/breakfast/{id:guid}")]
+        public IActionResult UpdateBreakfast(Guid id, UpdateRequest breakfastRequest)
+        {
+            return Ok(breakfastRequest);
+        }
+
+        [HttpDelete("/breakfast/{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+    }
+}
